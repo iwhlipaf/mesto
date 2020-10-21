10e9c7fabb8cd134f84d6729f0c56abb395095af
Andrey <iwhlipaf@yandex.ru>
2020-10-21 22:23:15 +0500

    sprint4_draft2

--- scripts/index.js
+++ scripts/index.js
@@ -3,32 +3,32 @@
 let popupOpenButton = document.querySelector('.profile__edit-button');
 let popupCloseButton = document.querySelector('.popup__close-button');
 let settingPopup = document.querySelector('.popup');
-let submitButton = document.querySelector('.input-form__submitbutton');
+let submitButton = document.querySelector('.input-form');
 
-let setDefaultName = document.querySelector('.popup__defaulttext_name')
-let setDefaultCareer = document.querySelector('.popup__defaulttext_career')
+let nameInput = document.querySelector('.input-form__fieldname')
+let jobInput = document.querySelector('.input-form__fieldcareer')
 
-let nameInput = document.querySelector('.input-form__field1')
-let jobInput = document.querySelector('.input-form__field2')
-
-let addNewName = document.querySelector('.profile__name')
-let addNewCareer = document.querySelector('.profile__career')
+let profileName = document.querySelector('.profile__name')
+let profileCareer = document.querySelector('.profile__career')
 
 function setPopup() {
-    settingPopup.classList.toggle('popup_active');
-    nameInput.value = setDefaultName.textContent;
-    jobInput.value = setDefaultCareer.textContent;
+    if (settingPopup.classList.contains('popup')) {
+        settingPopup.classList.toggle('popup_active');
+        nameInput.value = profileName.textContent;
+        jobInput.value = profileCareer.textContent;
+    } else {
+        settingPopup.classList.toggle('popup_active');
+    }
+}
+
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    profileName.textContent = nameInput.value;
+    profileCareer.textContent = jobInput.value;
+    setPopup()
 }
 
 popupOpenButton.addEventListener('click', setPopup);
 popupCloseButton.addEventListener('click', setPopup);
-
-function formSubmitHandler(evt) {
-    evt.preventDefault();
-    addNewName.textContent = nameInput.value;
-    addNewCareer.textContent = jobInput.value;
-    setPopup()
-}
-
-submitButton.addEventListener('click', formSubmitHandler);
+submitButton.addEventListener('submit', formSubmitHandler);
 
