893ac73c91715a912386687b0bb9cf5ba099130a
Andrey <iwhlipaf@yandex.ru>
2020-10-23 20:44:30 +0500

    sprint4_draft4

--- scripts/index.js
+++ scripts/index.js
@@ -5,18 +5,18 @@ let popupCloseButton = document.querySelector('.popup__close-button');
 let settingPopup = document.querySelector('.popup');
 let submitForm = document.querySelector('.input-form');
 
-let nameInput = document.querySelector('.input-form__field_name')
-let jobInput = document.querySelector('.input-form__field_career')
+let nameInput = document.querySelector('.input-form__field_info_name')
+let jobInput = document.querySelector('.input-form__field_info_career')
 
 let profileName = document.querySelector('.profile__name')
 let profileCareer = document.querySelector('.profile__career')
 
 function setPopup() {
-    settingPopup.classList.toggle('popup_active');
-    if (settingPopup.classList.contains('popup_active')) {
+    if (!settingPopup.classList.contains('popup_active')) {
         nameInput.value = profileName.textContent;
         jobInput.value = profileCareer.textContent;
     }
+    settingPopup.classList.toggle('popup_active');
 }
 
 function formSubmitHandler(evt) {
