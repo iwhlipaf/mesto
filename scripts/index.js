3fdcd8fb0f1cb808968ad49c3709b9a86fbc21a1
Andrey <iwhlipaf@yandex.ru>
2020-10-23 13:52:11 +0500

    sprint4_draft3

--- scripts/index.js
+++ scripts/index.js
@@ -3,21 +3,19 @@
 let popupOpenButton = document.querySelector('.profile__edit-button');
 let popupCloseButton = document.querySelector('.popup__close-button');
 let settingPopup = document.querySelector('.popup');
-let submitButton = document.querySelector('.input-form');
+let submitForm = document.querySelector('.input-form');
 
-let nameInput = document.querySelector('.input-form__fieldname')
-let jobInput = document.querySelector('.input-form__fieldcareer')
+let nameInput = document.querySelector('.input-form__field_name')
+let jobInput = document.querySelector('.input-form__field_career')
 
 let profileName = document.querySelector('.profile__name')
 let profileCareer = document.querySelector('.profile__career')
 
 function setPopup() {
-    if (settingPopup.classList.contains('popup')) {
-        settingPopup.classList.toggle('popup_active');
+    settingPopup.classList.toggle('popup_active');
+    if (settingPopup.classList.contains('popup_active')) {
         nameInput.value = profileName.textContent;
         jobInput.value = profileCareer.textContent;
-    } else {
-        settingPopup.classList.toggle('popup_active');
     }
 }
 
@@ -30,5 +28,5 @@ function formSubmitHandler(evt) {
 
 popupOpenButton.addEventListener('click', setPopup);
 popupCloseButton.addEventListener('click', setPopup);
-submitButton.addEventListener('submit', formSubmitHandler);
+submitForm.addEventListener('submit', formSubmitHandler);
 
